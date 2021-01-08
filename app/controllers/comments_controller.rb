8a9db2fc33011133f2ea30978c0506e79d3c2a73
dreadheaddreamz <github email address>
2021-01-07 19:37:52 -0600

    edited controllers and added routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,17 @@
 class CommentsController < ApplicationController
 
     def create
+        comment = Comment.create(params[:content])
+        render json: comment
     end
 
     def update
+        comment = Comment.update(params[:id])
+        comment.update
     end
 
     def destroy
+        comment = Comment.find_by(params[:id])
+        comment.delete
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -34,4 +34,15 @@ module GameCriticApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+        :max_age => 0
+    end
+  end
+
 end
