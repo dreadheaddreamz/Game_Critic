faad6fc1d033987191b92cfd3cad2216434bce5a
dreadheaddreamz <github email address>
2021-02-06 02:10:01 -0600

    fixing backend bugs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,29 @@
 class GamesController < ApplicationController
 
     def index
-        games = Game.all
+        @games = Game.all
 
-        render json: games, include: [:comments]
+        render json: @games, include: [:comments]
     end
 
     def show
-        game = Game.find_by_id(params[:id])
-        if !game 
-            render json: { error: "Not valid!", status: 400 }, status: 400
-        else
-        render json: game, include: [:comments]
-        end
-      end
+        @games = Game.find_by_id(params[:id])
+        render json: @games
+    end
 
-      def create
-        game = Game.new(game_params)
-        if !game
-            render json: { error: "Not valid game", status: 400 }, status: 400
-        else
-            render json: games, include: [:comments]
+    def create
+        @games = Game.new(game_params)
+        if @games.save
+            render json: @games, include: [:comments]          
+        else 
+            render json: {error: "We cant do that", status:400}
         end
-      end
+    end
+
 
       private
       def game_params
-        params.require(:game).permit(:title, :upvotes, :downvotes, :description, :image_url, :date)
+        params.require(:games).permit(:game_id, :title, :upvotes, :downvotes, :description, :image_url, :date)
       end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:index, :show, :create, :destroy]
-  resources :games, only: [:index, :show, :create]
+  resources :comments
+  resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
