feddee31840fc4d9d7650b81283b161a135e3d54
dreadheaddreamz <github email address>
2021-02-05 23:45:32 -0600

    added game post to controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,4 +5,28 @@ class GamesController < ApplicationController
 
         render json: games, include: [:comments]
     end
+
+    def show
+        game = Game.find_by_id(params[:id])
+        if !game 
+            render json: { error: "Not valid!", status: 400 }, status: 400
+        else
+        render json: game, include: [:comments]
+        end
+      end
+
+      def create
+        game = Game.new(game_params)
+        if !game
+            render json: { error: "Not valid game", status: 400 }, status: 400
+        else
+            render json: games, include: [:comments]
+        end
+      end
+
+      private
+      def game_params
+        params.require(:game).permit(:title, :upvotes, :downvotes, :description, :image_url, :date)
+      end
+
 end
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :description, :image_url, :upvotes, :downvotes
+  has_many :comments
 end
